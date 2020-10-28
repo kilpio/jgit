6b31daa12e744d6f7f8867fdc0a5993a30b8cfad
kilpio <kilpio@gmail.com>
2020-10-28 12:54:08 +0300

    new server depl

---
+++ new_jenkins_server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+JENKINS_USER=${JENKINS_USER:-jenkins}
+
+
+apt-get -y update && \
+apt-get -y upgrade && \
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" && \
+
+apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io
+
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose && \
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+apt-get -y install mc net-tools
+
+groupadd -g 1000 $JENKINS_USER
+useradd  -m -u 1000 -g 1000 -s /bin/bash $JENKINS_USER
+usermod -aG docker $JENKINS_USER
+
